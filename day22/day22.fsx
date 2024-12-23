51bc94e42b895f8b3b34162b2994e40802b0a76b
Rob Lenders <mental.blindfolds.on@gmail.com>
2024-12-22 19:10:05 -0600

    refactoring

--- day22/day22.fsx
+++ day22/day22.fsx
@@ -2,40 +2,15 @@ open System
 open System.IO
 
 let nextSecret (x : int64) =
-    let mix = (^^^)
     let prune x = x % 16777216L
     let x64 = x * 64L
-    let mixed = mix x x64
-    let x = prune mixed
+    let x = prune (x ^^^ x64)
 
     let xd32 = x / 32L
-    let mixed = mix x xd32
-    let x = prune mixed
+    let x = prune (x ^^^ xd32)
 
     let x2048 = x * 2048L
-    let mixed = mix x x2048
-    let x = prune mixed
-    x
-
-let tee f x = f x; x
-
-123L
-|> tee (printfn "%i")
-|>nextSecret 
-|> tee (printfn "%i")
-|>nextSecret 
-|> tee (printfn "%i")
-|>nextSecret 
-|> tee (printfn "%i")
-|>nextSecret 
-|> tee (printfn "%i")
-|>nextSecret 
-|> tee (printfn "%i")
-|>nextSecret 
-|> tee (printfn "%i")
-|>nextSecret 
-|> tee (printfn "%i")
-|>nextSecret 
+    prune (x ^^^ x2048)
 
 let sample = """1
 10
@@ -61,15 +36,12 @@ let nth next n seed =
     |> Seq.skip (n - 1)
     |> Seq.head
 
-let nthSecret = nth nextSecret
-
-
 let input = File.ReadAllText(Path.Combine(__SOURCE_DIRECTORY__, "input.txt"))
 
 let part1 input = 
     input
     |> parse
-    |> Array.sumBy (nthSecret 2000)
+    |> Array.sumBy (nth nextSecret 2000)
 
 #time
 let result1 = part1 input
@@ -77,6 +49,7 @@ let result1 = part1 input
 
 let price x = int (x % 10L)
 
+[<Struct>]
 type History = History of d0: int * d1: int * d2: int * d3: int
 
 let packedHistory (History(d0, d1, d2, d3)) =
@@ -85,10 +58,6 @@ let packedHistory (History(d0, d1, d2, d3)) =
     d0 + 9 + ((d1 + 9) <<< 5) + ((d2 + 9) <<< 10) + ((d3 + 9) <<< 15)
 
 let priceHistory seed = 
-    let n0 = seed
-    let n1 = nextSecret n0
-    let n2 = nextSecret n1
-    let n3 = nextSecret n2
     let rec prices p0 p1 p2 p3 n3 = 
         seq {
             let n4 = nextSecret n3
@@ -102,6 +71,10 @@ let priceHistory seed =
             yield! prices p1 p2 p3 p4 n4
         }
 
+    let n0 = seed
+    let n1 = nextSecret n0
+    let n2 = nextSecret n1
+    let n3 = nextSecret n2
     prices (price n0) (price n1) (price n2) (price n3) n3
 
 let part2 input =
@@ -114,11 +87,10 @@ let part2 input =
             seed 
             |> priceHistory 
             |> Seq.take 1997 
-            |> Array.ofSeq
-            |> Array.distinctBy snd
             |> Seq.map (fun (v, k) -> packedHistory k, v)
+            |> Seq.distinctBy fst
             |> dict
-            )
+        )
 
     let priceChanges = 
         histories
@@ -130,15 +102,13 @@ let part2 input =
     printfn $"priceChanges count {priceChanges.Length}"
 
     priceChanges
-    |> Array.Parallel.mapi (fun i change ->
-        let x = 
-            histories
-            |> Array.sumBy (fun history ->
-                match history.TryGetValue change with
-                | true, x -> int64 x
-                | false, _ -> int64 0
-            )
-        x
+    |> Array.Parallel.map (fun change ->
+        histories
+        |> Array.sumBy (fun history ->
+            match history.TryGetValue change with
+            | true, x -> int64 x
+            | false, _ -> int64 0
+        )
     )
     |> Array.max
 
@@ -147,4 +117,3 @@ let result2 = part2 input
 #time
 
 printfn $"Part1: {result1} Part2: {result2}"
-
